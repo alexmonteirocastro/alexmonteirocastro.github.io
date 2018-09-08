b4a44606ccee7ec9602953df2c32cc85cc292e18
Alexandre Castro <alex.monteiro@seznam.cz>
2018-09-08 21:36:27 +0200

    video list

--- youtube-api/main.js
+++ youtube-api/main.js
@@ -102,10 +102,28 @@ function getChannel(channel) {
                 <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${channel.snippet.customUrl}">Visit Channel</a>
             `;
             showChannelData(output);
+
+            const playlistId = channel.contentDetails.relatedPlaylists.uploads;
+            requestVideoPlaylist(playlistId);
         })
         .catch(err => alert('No channel by the name of ' + channel))
 }
 
+// Add commas to number
 function numberWithCommas(x){
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function requestVideoPlaylist(playlistId){
+    const requestOptions = {
+        playlistId: playlistId,
+        part: 'snippet',
+        maxResults: 10
+    }
+
+    const request = gapi.client.youtube.playlistItems.list(requestOptions);
+
+    request.execute(response => {
+        console.log(response)
+    });
 }
